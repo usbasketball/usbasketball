7529436916363d6f552abf7d523705225073ddec
chengh42 <hi@hcheng.nl>
2026-08-17 19:36:49 +0200

    Add Sentry integration: error monitoring, tracing, logs

---
+++ sentry.server.config.ts
@@ -0,0 +1,9 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://d70dc81b3ef1c677c8a3016cea7f80fe@o4511898827161600.ingest.de.sentry.io/4511909338349648",
+
+  tracesSampleRate: process.env.NODE_ENV === "development" ? 1.0 : 0.1,
+
+  enableLogs: true,
+});
